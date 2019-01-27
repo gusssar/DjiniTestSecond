7d21fb41bf7224d8c1a621097bf8a4122051750c
gusssar <dassler47@gmail.com>
2019-01-27 15:22:07 +0300

    sec-list-done

--- index.js
+++ index.js
@@ -8,82 +8,127 @@ let dnssec = document.getElementById('dnssec');
 let redirect = document.getElementById('redirect');
 
 
-businesscard.onclick = function () {
-        let state = businesscard.childNodes[3].style.display;
-        console.log(state);
-            if (state ==='none'){
-                businesscard.childNodes[3].style.display='block';
-            }else {
-                businesscard.childNodes[3].style.display='none';
+
+    function CleanLi(){
+        let item_lenght=document.getElementById('item-list').children.length;
+
+        for (let i=0; i<item_lenght; i++){
+            if(document.getElementById('item-list').children[i].childNodes[3].style.display ==='block'){
+                document.getElementById('item-list').children[i].childNodes[3].style.display ='none';
+                document.getElementById('item-list').children[i].style.marginBottom='';
             }
-    };
+        }
+    }
+
+businesscard.onclick = function () {
+    let state = businesscard.childNodes[3].style.display;
+
+    if (state ==='block'){
+        CleanLi();
+        businesscard.childNodes[3].style.display='none';
+        businesscard.style.marginBottom = '';
+    }else {
+        CleanLi();
+        businesscard.childNodes[3].style.display='block';
+        businesscard.style.marginBottom = '154px';
+    }
+};
 
 dns.onclick = function () {
     let state = dns.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        dns.childNodes[3].style.display='block';
-    }else {
+
+    if (state ==='block'){
+        CleanLi();
         dns.childNodes[3].style.display='none';
+        dns.style.marginBottom = '';
+    }else {
+        CleanLi();
+        dns.childNodes[3].style.display='block';
+        dns.style.marginBottom = '154px';
     }
 };
 
 certificate.onclick = function () {
     let state = certificate.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        certificate.childNodes[3].style.display='block';
-    }else {
+
+    if (state ==='block'){
+        CleanLi();
         certificate.childNodes[3].style.display='none';
+        certificate.style.marginBottom = '';
+    }else {
+        CleanLi();
+        certificate.childNodes[3].style.display='block';
+        certificate.style.marginBottom = '115px';
     }
 };
 
 autorenewal.onclick = function () {
     let state = autorenewal.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        autorenewal.childNodes[3].style.display='block';
-    }else {
+
+    if (state ==='block'){
+        CleanLi();
         autorenewal.childNodes[3].style.display='none';
+        autorenewal.style.marginBottom = '';
+    }else {
+        CleanLi();
+        autorenewal.childNodes[3].style.display='block';
+        autorenewal.style.marginBottom = '115px';
     }
 };
 
 cloud.onclick = function () {
     let state = cloud.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        cloud.childNodes[3].style.display='block';
+
+    if (state ==='block'){
+        CleanLi();
+    cloud.childNodes[3].style.display='none';
+    cloud.style.marginBottom = '';
     }else {
-        cloud.childNodes[3].style.display='none';
+        CleanLi();
+    cloud.childNodes[3].style.display='block';
+    cloud.style.marginBottom = '135px';
     }
 };
 
 freessl.onclick = function () {
     let state = freessl.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        freessl.childNodes[3].style.display='block';
+
+    if (state ==='block'){
+        CleanLi();
+    freessl.childNodes[3].style.display='none';
+    freessl.style.marginBottom = '';
     }else {
-        freessl.childNodes[3].style.display='none';
+        CleanLi();
+    freessl.childNodes[3].style.display='block';
+    freessl.style.marginBottom = '175px';
     }
+
 };
 
 dnssec.onclick = function () {
     let state = dnssec.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        dnssec.childNodes[3].style.display='block';
+
+    if (state ==='block'){
+        CleanLi();
+    dnssec.childNodes[3].style.display='none';
+    dnssec.style.marginBottom = '';
     }else {
-        dnssec.childNodes[3].style.display='none';
+        CleanLi();
+    dnssec.childNodes[3].style.display='block';
+    dnssec.style.marginBottom = '155px';
     }
 };
 
 redirect.onclick = function () {
     let state = redirect.childNodes[3].style.display;
-    console.log(state);
-    if (state ==='none'){
-        redirect.childNodes[3].style.display='block';
+
+    if (state ==='block'){
+        CleanLi();
+    redirect.childNodes[3].style.display='none';
+    redirect.style.marginBottom = '';
     }else {
-        redirect.childNodes[3].style.display='none';
+        CleanLi();
+    redirect.childNodes[3].style.display='block';
+    redirect.style.marginBottom = '175px';
     }
 };
